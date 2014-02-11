ad5a399c694dcbff18795d077d00ff6034057040
Chris Beer <cabeer@stanford.edu>
2014-02-11 08:52:07 -0800

    change the view configuration key from table to data_grid, to avoid triggering some private jruby behaviors

--- lib/blacklight/table/engine.rb
+++ lib/blacklight/table/engine.rb
@@ -3,8 +3,8 @@ require 'blacklight'
 module Blacklight
   module Table
     class Engine < Rails::Engine
-      Blacklight::Configuration.default_values[:view].table.icon_class = "glyphicon-calendar"
-      Blacklight::Configuration.default_values[:view].table.partials = []
+      Blacklight::Configuration.default_values[:view].data_grid.icon_class = "glyphicon-calendar"
+      Blacklight::Configuration.default_values[:view].data_grid.partials = []
     end
   end
 end
